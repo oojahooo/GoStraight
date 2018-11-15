2437a477a30491d3792b5943e03b3c33714ef5f0
oojahooo <oojahooo@gmail.com>
2018-11-15 15:01:17 +0900

    Add Map centerPoint and Marker

--- app/src/main/java/com/oojahooo/gostraight/MapFragment.java
+++ app/src/main/java/com/oojahooo/gostraight/MapFragment.java
@@ -6,6 +6,8 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import net.daum.mf.map.api.MapPOIItem;
+import net.daum.mf.map.api.MapPoint;
 import net.daum.mf.map.api.MapView;
 
 public class MapFragment extends Fragment {
@@ -17,7 +19,17 @@ public class MapFragment extends Fragment {
         View v = inflater.inflate(R.layout.fragment_map, container, false);
         MapView mapView = new MapView(getActivity());
         ViewGroup mapViewContainer = (ViewGroup)v.findViewById(R.id.map_view);
+        MapPoint mapPoint = MapPoint.mapPointWithGeoCoord(37.5864371, 127.029278);
+        mapView.setMapCenterPoint(mapPoint, true);
         mapViewContainer.addView(mapView);
+
+        MapPOIItem marker = new MapPOIItem();
+        marker.setItemName("안암역테스트");
+        marker.setTag(0);
+        marker.setMapPoint(mapPoint);
+        marker.setMarkerType(MapPOIItem.MarkerType.BluePin);
+        marker.setSelectedMarkerType(MapPOIItem.MarkerType.RedPin);
+        mapView.addPOIItem(marker);
         return v;
     }
 }
