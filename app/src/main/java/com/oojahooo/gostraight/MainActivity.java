38da5d1496ca6e7f56dfbe2e578fd4c79736742c
oojahooo <oojahooo@gmail.com>
2018-12-11 17:14:33 +0900

    Edit and Add release APK

--- app/src/main/java/com/oojahooo/gostraight/MainActivity.java
+++ app/src/main/java/com/oojahooo/gostraight/MainActivity.java
@@ -10,16 +10,13 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.Spinner;
-import android.widget.Toast;
 
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
